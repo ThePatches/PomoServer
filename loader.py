d1ce8545e145fcdeb5e6188e55fb70f6839fe273
ThePatches <warder05@gmail.com>
2011-03-30 23:51:26 -0400

    Everything is working? We'll have to see!

--- loader.py
+++ loader.py
@@ -1,7 +1,27 @@
-from pom1 import Task
+from task import Task, TasQue
 
-t_list = []
-f = open('first.dat', 'r')
-t_dat = f.read()
-print t_dat
-f.close()
+# t_list = []
+# f = open('first.dat', 'r')
+# t_dat = f.read()
+# print t_dat
+# f.close()
+
+def loadFile(iFile): # Should be a class
+    f = open(iFile, 'r')
+    tasques = []
+    for line in f:
+        r = False
+        t = 0
+        if line[0] != '#':
+            s1 = line.split('=')
+            s2 = s2.split('|')
+            if s2[2] == 'recur':
+                r = True
+                if s[3] == 'inf':
+                    t = -1
+                else:
+                    t = int(s[3])
+
+            task = Task(s[0], s[1], r, t)
+        tasques.append(task)
+    q = TasQue(tasques)
--- task.py
+++ task.py
@@ -1,20 +1,36 @@
-import time
+# import time
+from collections import deque
+
+# Task Class
 
 class Task:
     name = "none"
     comment = "there is nothing about this task..."
     complete = False
-    def __init__(self, iName="none", iComment="nada!"):
+    recur = False
+    times = 1
+    def __init__(self, iName="none", iComment="nada!",
+                 iRecur=False, iTimes=1):
         self.name = iName
         self.comment=iComment
         complete = False
+        self.recur = iRecur
+        self.times = iTimes
+        
     def done(self):
         self.complete = True
+        if self.recur == True and self.times > 0:
+            self.times = self.times - 1
         
+# Task Queue?
+class TasQue:
+    __thequeue = []
 
-def Demo():
-    t = Task("Something")    
-    print 'This is something.'
-    time.sleep(20)
-    print t.name + ' is now done.'
-    t.done()
+    def __init__(self, ilist):
+        __thequeue = deque(ilist)
+
+    def mark_complete(self):
+        t = __thequeue.popleft()
+        t.done()
+        if t.recur == True and t.times != 0:
+            __thequeue.append(t)
